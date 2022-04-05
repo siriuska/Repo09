3e448be6211d8c01f164980a66405bde8324fccd
siriuska <you@example.com>
2022-04-06 00:47:30 +0200

    Generating random whole numbers

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,5 +1,5 @@
 function randomRangeNumber(minNumber, maxNumber) {
-    return Math.floor(Math.random() * (minNumber - maxNumber + 1) + minNumber);
+  return Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
 }
 
 console.log(randomRangeNumber(15, 20));
