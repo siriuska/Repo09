c08df16fd75e5dfa68df1dd30bf20c189b2cdca7
siriuska <you@example.com>
2022-04-05 14:39:19 +0200

    Equality and strict equality operator

---
+++ ex03/src/index.js
@@ -0,0 +1,23 @@
+function myEqualFunction(num) {
+    if(num === 23){
+        return "Equal";
+    }
+    return "Not equal";
+}
+
+console.log(myEqualFunction(5));
+myEqualFunction(5);
+
+console.log(myEqualFunction(23));
+myEqualFunction(23);
+
+console.log(myEqualFunction("23"));
+myEqualFunction("23");
+
+console.log(myEqualFunction('23'));
+myEqualFunction('23');
+
+console.log(myEqualFunction("text"));
+myEqualFunction("text");
+
+module.exports = myEqualFunction;
