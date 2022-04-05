9fb5a6a5b5782474b019348cb648441f6667cf5b
siriuska <you@example.com>
2022-04-06 01:48:16 +0200

    Switch statements

--- ex15/src/index.js
+++ ex15/src/index.js
@@ -1,20 +1,20 @@
 function switchCase(letter) {
-    var animals = "";
-    switch(letter) {
-        case "a":
-            animals = "antelope";
-            break;
-        case "b":
-            animals = "bird";
-            break;
-        case "c":
-            animals = "cat";
-            break;
-        default:
-            animals = "stuff"
-            break;
-    }
-    return animals;
+  var animals = "";
+  switch (letter) {
+    case "a":
+      animals = "antelope";
+      break;
+    case "b":
+      animals = "bird";
+      break;
+    case "c":
+      animals = "cat";
+      break;
+    default:
+      animals = "stuff";
+      break;
+  }
+  return animals;
 }
 
 console.log(switchCase("a"));
