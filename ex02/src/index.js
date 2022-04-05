dbd50b46bbf520baaafe0125e47953ed902f5933
siriuska <you@example.com>
2022-04-05 14:29:44 +0200

    If statements

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+function myFunction(myParameter) {
+    if(myParameter) {
+        return "The parameter is true!";
+    }
+    return "The parameter is false!";
+}
+
+console.log(myFunction(true));
+myFunction(true);
+console.log(myFunction(false));
+myFunction(false);
+
+module.exports = myFunction;
