41304f5dac70e2182e8e888332b7d8969aefa8cb
siriuska <you@example.com>
2022-04-05 17:01:41 +0200

    Comparison operators

---
+++ ex06/src/index.js
@@ -0,0 +1,19 @@
+function comparisonToEqual(a) {
+    if(a < 5) {
+        return "Less than 5";
+    }
+    if(a < 10) {
+        return "Less than 10";
+    }
+    if(a >= 10 && a <= 20) {
+        return "10 or over";
+    }
+    return "More than 20";
+}
+
+console.log(comparisonToEqual(0));
+console.log(comparisonToEqual(5));
+console.log(comparisonToEqual(17));
+console.log(comparisonToEqual(21));
+
+module.exports = comparisonToEqual;
