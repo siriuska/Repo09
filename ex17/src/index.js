50176eb4d0f3ae1466847e196eb5999e5d0e3270
siriuska <you@example.com>
2022-04-06 01:49:09 +0200

    While loops

--- ex17/src/index.js
+++ ex17/src/index.js
@@ -1,13 +1,12 @@
 function inverseWhile() {
-    var fiveNumbers = "";
-    var i = 5;
+  var fiveNumbers = "";
+  var i = 5;
 
-    while (i >= 0) {
-
-        (i === 0) ? fiveNumbers += i : fiveNumbers += i + ",";
-        i--;
-    }
-    return fiveNumbers
+  while (i >= 0) {
+    i === 0 ? (fiveNumbers += i) : (fiveNumbers += i + ",");
+    i--;
+  }
+  return fiveNumbers;
 }
 
 console.log(inverseWhile());
